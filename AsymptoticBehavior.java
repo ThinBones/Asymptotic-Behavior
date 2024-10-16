a7f4ab84d66cff60c508f75187c6032ecea0cba3
ThinBones <145872474+ThinBones@users.noreply.github.com>
2024-10-16 18:15:39 -0500

    Update AsymptoticBehavior.java

--- AsymptoticBehavior.java
+++ AsymptoticBehavior.java
@@ -5,18 +5,33 @@ import java.util.*;
 
 public class AsymptoticBehavior {
     private static int target;
-    double currentTimeMillis = System.currentTimeMillis();
+    private static double localTimeMillis;
+
 
     public static void main(String[] a) throws FileNotFoundException {
-        int[] randArr   = generateRandomArray();
-        int[] sortedArr = generateSortedArray();
+        localTimeMillis = System.currentTimeMillis();
 
-//        for(int i = 0; i < 100001; i++) {
-//            System.out.println(sortedArr[i]);
-//        }
+        int[] randArr   = generateRandomArray();
+        System.out.println("Generating random array: " + localTimeMillis);
+
+        int[] sortedArr = generateSortedArray();
+        System.out.println("Generating sorted array: " + localTimeMillis);
+
+        sortArray(randArr);
+        System.out.println("Sorting random array: " + localTimeMillis);
+
+        getTarget();
+        binarySearch(randArr, target);
+        System.out.println("Searching for target: " + localTimeMillis);
     }
 
+    /**
+     * Creates an array with 100000 indexes with random integers in
+     * each index
+     * @return
+     */
     public static int[] generateRandomArray() {
+        double currentTimeMillis = System.currentTimeMillis();
         Random random = new Random();
         int[]  intArr = new int[100000];
 
@@ -25,20 +40,35 @@ public class AsymptoticBehavior {
             intArr[i] = randInt;
         }
 
+        double newTimeMillis = System.currentTimeMillis();
+        localTimeMillis = newTimeMillis - currentTimeMillis;
         return intArr;
     }
 
+    /**
+     * Generatese an array with 100000 indexes with increasing integers
+     * in each index
+     * @return
+     */
     public static int[] generateSortedArray() {
+        double currentTimeMillis = System.currentTimeMillis();
         int[] intArr = new int[100000];
 
         for(int i = 0; i < 100000; i++) {
             intArr[i] = i;
         }
 
+        double newTimeMillis = System.currentTimeMillis();
+        localTimeMillis = newTimeMillis - currentTimeMillis;
         return intArr;
     }
 
+    /**
+     * Sorts an array via bubble sort
+     * @param arr
+     */
     public static void sortArray(int[] arr) {
+        double currentTimeMillis = System.currentTimeMillis();
         int n = arr.length;
         int temp = 0;
         for(int i = 0; i < n; i++){
@@ -51,9 +81,16 @@ public class AsymptoticBehavior {
                 }
             }
         }
+        double newTimeMillis = System.currentTimeMillis();
+        localTimeMillis = newTimeMillis - currentTimeMillis;
     }
 
+    /**
+     * Finds a target integer from a text file
+     * @throws FileNotFoundException
+     */
     public static void getTarget() throws FileNotFoundException {
+        double currentTimeMillis = System.currentTimeMillis();
         FileReader reader;
         String fileInputName = "data.txt";
         Scanner scan = null;
@@ -61,10 +98,19 @@ public class AsymptoticBehavior {
         scan = new Scanner(new BufferedReader(new FileReader(fileInputName)));
         int int1 = scan.nextInt();
 
+        double newTimeMillis = System.currentTimeMillis();
+        localTimeMillis = newTimeMillis - currentTimeMillis;
         target = int1;
     }
 
-    public static boolean binarySearch(int[] arr) {
+    /**
+     * Performs a binary search (cutting a selection in half
+     * until the target is found (or where it should be)
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static boolean binarySearch(int[] arr, int target) {
         int low = 0, high = arr.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
@@ -77,13 +123,13 @@ public class AsymptoticBehavior {
             if (arr[mid] < target)
                 low = mid + 1;
 
-                // If x is smaller, ignore right half
+            // If x is smaller, ignore right half
             else
                 high = mid - 1;
         }
 
-        // If we reach here, then element was
-        // not present
+        double newTimeMillis = System.currentTimeMillis();
+        localTimeMillis -= newTimeMillis;
         return false;
     }
 }
