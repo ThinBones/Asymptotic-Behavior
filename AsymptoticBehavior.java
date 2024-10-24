56f3d9a8b0ea212f4ad09a34ef7930f82175d5a7
ThinBones <145872474+ThinBones@users.noreply.github.com>
2024-10-24 10:12:55 -0500

    Update AsymptoticBehavior.java
    fix javadoc at the bottom

--- AsymptoticBehavior.java
+++ AsymptoticBehavior.java
@@ -1,3 +1,14 @@
+/**
+ * Author:      Jack Pender, Emma Holt
+ * Date:        October 2024
+ * Class:       APCSA
+ * Description: Creates 2 arrays, one random and one pre-sorted,
+ *              then sorts the random array and searched for a target
+ *              in an array. Finds the time it takes for each
+ *              process and the time for the whole main method
+ *              to execute.
+ */
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,6 +20,7 @@ public class AsymptoticBehavior {
 
 
     public static void main(String[] a) throws FileNotFoundException {
+        double currentTimeMillis = System.currentTimeMillis();
         localTimeMillis = System.currentTimeMillis();
 
         int[] randArr   = generateRandomArray();
@@ -23,19 +35,24 @@ public class AsymptoticBehavior {
         getTarget();
         binarySearch(randArr, target);
         System.out.println("Searching for target: " + localTimeMillis);
+
+        double newTimeMillis = System.currentTimeMillis();
+        localTimeMillis = newTimeMillis - currentTimeMillis;
+
+        System.out.println("Main method: " + localTimeMillis);
     }
 
     /**
-     * Creates an array with 100000 indexes with random integers in
+     * Generates an array with 100000 indexes with random integers in
      * each index
      * @return
      */
     public static int[] generateRandomArray() {
         double currentTimeMillis = System.currentTimeMillis();
         Random random = new Random();
-        int[]  intArr = new int[100000];
+        int[]  intArr = new int[5000000];
 
-        for(int i = 0; i < 100000; i++) {
+        for(int i = 0; i < 5000000; i++) {
             int randInt = random.nextInt(10000);
             intArr[i] = randInt;
         }
@@ -46,15 +63,15 @@ public class AsymptoticBehavior {
     }
 
     /**
-     * Generatese an array with 100000 indexes with increasing integers
+     * Generates an array with 1000000 indexes with increasing integers
      * in each index
      * @return
      */
     public static int[] generateSortedArray() {
         double currentTimeMillis = System.currentTimeMillis();
-        int[] intArr = new int[100000];
+        int[] intArr = new int[5000000];
 
-        for(int i = 0; i < 100000; i++) {
+        for(int i = 0; i < 5000000; i++) {
             intArr[i] = i;
         }
 
@@ -74,7 +91,6 @@ public class AsymptoticBehavior {
         for(int i = 0; i < n; i++){
             for(int j = 1; j < (n - i); j++){
                 if(arr[j-1] > arr[j]){
-                    //swap elements
                     temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
@@ -115,15 +131,12 @@ public class AsymptoticBehavior {
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            // Check if x is present at mid
             if (arr[mid] == target)
                 return true;
 
-            // If x greater, ignore left half
             if (arr[mid] < target)
                 low = mid + 1;
 
-            // If x is smaller, ignore right half
             else
                 high = mid - 1;
         }
@@ -133,3 +146,45 @@ public class AsymptoticBehavior {
         return false;
     }
 }
+
+/**
+ * 1.
+ *     i.
+ *     O(n) for random: O(n) = n
+ *     O(n) for pre-sorted: O(n) = n
+ *     ii.
+ *     t1 random = 14.0ms
+ *     t1 pre-sorted = 4.0ms
+ *     iv.
+ *     t2 random = 53.0ms
+ *     t2 pre-sorted = 10.0ms
+ *     v.
+ *     t2 / t1 random =
+ *     t2 / t1 pre-sorted =
+ *     How does this relate to O(n) for random
+ *
+ *     How does this relate to O(n) for pre-sorted
+ *
+ * 2.
+ *     i.
+ *     t1 = 1172504.0ms
+ *     t2 = 3.0107E7ms
+ *     t2 / t1 =
+ *     
+ * 3.
+ *     n is the same as the array being searched (100000)
+ *     t1 = 10.0ms
+ *     t2 = 43.0ms
+ *     t2 / t1 =
+ * 4.
+ *     Main Method:
+ *     Generate Random Array:
+ *     Generate Sorted Array:
+ *     Sort Random Array:
+ *     Search Array:
+ * 5.
+ *     The main discrepency is that simulations
+ *     find T(n) as opposed to O(n) which shows
+ *     the real time of the simulation as opposed
+ *     to the worst-case-scenario.
+ */
